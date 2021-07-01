a2a4d291a1ec945c278a77763d5284b07ff7b630
Worawit <worawit.bs@gmail.com>
2021-07-02 01:02:37 +0700

    Edit data user response

--- app/featues/user/model/user.go
+++ app/featues/user/model/user.go
@@ -3,8 +3,10 @@ package model
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	Id       primitive.ObjectID `bson:"_id" json:"id"`
-	Username string             `bson:"username" json:"username"`
-	Password string             `bson:"password" json:"-"`
-	Role     string             `bson:"role" json:"role"`
+	Id        primitive.ObjectID `bson:"_id" json:"id"`
+	FirstName string             `bson:"firstName" json:"firstName"`
+	LastName  string             `bson:"lastName" json:"lastName"`
+	Username  string             `bson:"username" json:"username"`
+	Password  string             `bson:"password" json:"-"`
+	Role      string             `bson:"role" json:"role"`
 }
